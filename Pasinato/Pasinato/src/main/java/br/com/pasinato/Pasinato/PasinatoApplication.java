fc507c9b903b06047b5d8c0f691ff60caeb7cf1f
Marcio Oliveira <marciooliveirasantos2012@gmail.com>
2023-11-10 15:35:42 -0300

    primeiro commit estrutura do projeto em java

---
+++ Pasinato/Pasinato/src/main/java/br/com/pasinato/Pasinato/PasinatoApplication.java
@@ -0,0 +1,13 @@
+package br.com.pasinato.Pasinato;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PasinatoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PasinatoApplication.class, args);
+	}
+
+}
